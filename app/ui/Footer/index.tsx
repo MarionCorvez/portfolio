68a43d28ab24cd6f4e2f626a4cf9bd287f467d1d
Marion Corvez <marion.corvez@gmail.com>
2024-07-28 11:15:40 +0200

    Ajout d'un indicateur de performance

--- app/ui/Footer/index.tsx
+++ app/ui/Footer/index.tsx
@@ -1,4 +1,5 @@
 import styles from "./index.module.css";
+import Image from "next/image";
 import Contact from "@/app/ui/Contact";
 
 export default function Footer() {
@@ -7,6 +8,19 @@ export default function Footer() {
       <div className={`${styles.container} flex--center screen`}>
         <span>© 2024 - Marion Corvez</span>
         <Contact />
+        <a
+          href="https://bff.ecoindex.fr/redirect/?url=https://www.ecoindex.fr/en/result/?id=1cea285d-5c1a-42f2-b190-9e2bddb368c9"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="https://bff.ecoindex.fr/badge/?theme=dark&url=https://portfolio-naotas-projects.vercel.app/"
+            alt="Ecoindex Badge"
+            width={108}
+            height={32}
+            className={styles.ecoindex}
+          />
+        </a>
       </div>
     </footer>
   );
