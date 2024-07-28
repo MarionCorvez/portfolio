2bece80c337bbbea790674019253df2984161a22
Marion Corvez <marion.corvez@gmail.com>
2024-07-28 10:40:20 +0200

    Ajout d'un useEffect pour corriger la gestion de Window de Nextjs

--- app/ui/Switchmode/index.tsx
+++ app/ui/Switchmode/index.tsx
@@ -1,20 +1,24 @@
+import { useEffect } from "react";
+
 function Switchmode() {
   // Detect if dark mode is on
-  const useDark = window.matchMedia("(prefers-color-scheme: dark)");
-  // Toggle the "theme--dark" class based on if the media query matches
-  function toggleDarkMode(state: boolean | undefined) {
-    document.documentElement.classList.toggle("theme--dark", state);
-  }
-  // Initial setting
-  toggleDarkMode(useDark.matches);
+  useEffect(() => {
+    const useDark = window.matchMedia("(prefers-color-scheme: dark)");
+    // Toggle the "theme--dark" class based on if the media query matches
+    function toggleDarkMode(state: boolean | undefined) {
+      document.documentElement.classList.toggle("theme--dark", state);
+    }
+    // Initial setting
+    toggleDarkMode(useDark.matches);
 
-  // Listen for changes in the OS settings
-  useDark.addEventListener("change", (event) => {
-    toggleDarkMode(event.matches);
-  });
+    // Listen for changes in the OS settings
+    useDark.addEventListener("change", (event) => {
+      toggleDarkMode(event.matches);
+    });
+  }, []);
 
   // Toggle the "theme--dark" class on click
-  function handleMode() {
+  function handleMode(): void {
     document.documentElement.classList.toggle("theme--dark");
   }
 
